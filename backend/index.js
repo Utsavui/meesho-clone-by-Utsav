fe01379d72a98544e2c2824de31d60483a9e41e7
Utsavui <utsavgupta1356@gmail.com>
2025-10-15 19:18:46 +0530

    update backend

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,6 @@ import fs from "fs";
 import path from "path";
 import bodyParser from "body-parser";
 const app = express();
-const port =process.env.PORT || 4000
 app.use(cors());  // CORS enable
 app.use(express.json());
 app.use(bodyParser.json())
